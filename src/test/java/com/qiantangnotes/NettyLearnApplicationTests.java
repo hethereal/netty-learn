6b5f3bd15a5b124f6166b7dc1f92bb062b302b31
Bryce <Plato_ideal@outlook.com>
2025-08-24 02:18:52 +0800

    init

---
+++ src/test/java/com/qiantangnotes/NettyLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiantangnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
