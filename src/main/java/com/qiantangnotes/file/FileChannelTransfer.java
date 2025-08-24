ca1b43c6e08faab946c6ea116e1cda8e5ab5b541
Bryce <Plato_ideal@outlook.com>
2025-08-24 12:38:54 +0800

    文件编程

---
+++ src/main/java/com/qiantangnotes/file/FileChannelTransfer.java
@@ -0,0 +1,20 @@
+package com.qiantangnotes.file;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+
+public class FileChannelTransfer {
+    public static void main(String[] args) {
+        try (FileChannel from = new FileInputStream("data.txt").getChannel();
+             FileChannel to = new FileOutputStream("data3.txt").getChannel()) {
+            //效率高
+            long size = from.size();
+            for (long left = size; left > 0;) {
+                left -= from.transferTo(0, size, to);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/qiantangnotes/file/WalkFileTreeTest.java
@@ -0,0 +1,28 @@
+package com.qiantangnotes.file;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class WalkFileTreeTest {
+    public static void main(String[] args) throws IOException {
+        AtomicInteger dirCount = new AtomicInteger(0);
+        AtomicInteger fileCount = new AtomicInteger(0);
+        Files.walkFileTree(Paths.get("/Users/source"), new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                dirCount.incrementAndGet();
+                return super.preVisitDirectory(dir, attrs);
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                fileCount.incrementAndGet();
+                return super.visitFile(file, attrs);
+            }
+        });
+        System.out.println("dir count: " + dirCount.get());
+        System.out.println("file count: " + fileCount.get());
+    }
+}
